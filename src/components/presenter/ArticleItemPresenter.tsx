fd72fa3cc58ded23fca57e03bc8d73dae72663a8
Preslav Rachev <rachev.preslav@gmail.com>
2017-05-17 07:20:34 +0200

    Display a preview image for every article (with a default placeholder)

--- src/components/presenter/ArticleItemPresenter.tsx
+++ src/components/presenter/ArticleItemPresenter.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import {
+  Image,
   Text,
   View
 } from 'react-native';
 import { Card } from 'react-native-material-design';
+import { MediumConstants } from '../../constants/MediumConstants';
 
 import ArticleBasicInfo from '../../model/ArticleBasicInfo';
 
 const ArticleItemPresenter = ({info}: {info: ArticleBasicInfo}) => {
+  const DEFAULT_IMAGE_URL = 'https://facebook.github.io/react/img/logo_og.png'
+  const imageUrl = (info.virtuals && info.virtuals.previewImage) ?
+    MediumConstants.MEDIUM_CDN_URL + info.virtuals.previewImage.imageId :
+    DEFAULT_IMAGE_URL;
+
   return (
     <View>
       <Card>
+        <Card.Media
+            image={ <Image source={{uri: imageUrl}} /> }
+            overlay
+        />
         <Card.Body>
           <Text>{info.title}</Text>
         </Card.Body>
---
+++ src/constants/MediumConstants.ts
@@ -0,0 +1,6 @@
+/**
+ * MediumConstants
+ */
+export namespace MediumConstants {
+  export const MEDIUM_CDN_URL: string = 'https://cdn-images-1.medium.com/max/400/';
+}
